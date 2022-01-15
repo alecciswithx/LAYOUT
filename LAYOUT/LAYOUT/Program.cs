5024689afbb4919bdb17271aac151f4e59dd2790
alecciswithx <repositorioalecciswithx@gmail.com>
2022-01-15 09:44:58 -0600

    alecciswithx
    Create proyect :)

---
+++ LAYOUT/LAYOUT/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LAYOUT.Utilerias;
+
+namespace LAYOUT
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Printer Print = new Printer();
+            Print.DrawDogMessage();
+
+            string titulo = Console.ReadLine();
+            Print.PrintTitle(titulo);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LAYOUT/LAYOUT/Utilerias/Printer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAYOUT.Utilerias
+{
+    class Printer
+    {
+        public void DrawDogMessage()
+        {
+            Console.WriteLine("          __   _,--=\"=--,_   __");
+            Console.WriteLine("         /  \\.\"    .-.    \"./  \\");
+            Console.WriteLine("        /  ,/  _   : :   _  \\/` \\");
+            Console.WriteLine("        \\  `| /o\\  :_:  /o\\ |\\__/");
+            Console.WriteLine("         `-'| :=\"~` _ `~\"=: |");
+            Console.WriteLine("            \\`     (_)     `/");
+            Console.WriteLine("    .-\" -.   \\      |      /   .- \"-.");
+            Console.WriteLine(".---{     }--|  /,.-'-.,\\  |--{     }---.");
+            Console.WriteLine(" )  (_)_)_)  \\_/`~-===-~`\\_/  (_(_(_)  (");
+            Console.WriteLine("============================================");
+            Console.WriteLine("||                                        ||");
+            Console.WriteLine("||        Suscribete a mi canal :3        ||");
+            Console.WriteLine("||  Pulsa enter para limpiar la pantala   ||");
+            Console.WriteLine("============================================");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void PrintTitle(string ptitulo)
+        {
+            string tituloFormateado = $"************* {ptitulo} *************";
+            Console.WriteLine("".PadLeft(tituloFormateado.Length, '='));
+            Console.WriteLine(tituloFormateado);
+            Console.WriteLine("".PadRight(tituloFormateado.Length, '='));
+        }
+    }
+}
